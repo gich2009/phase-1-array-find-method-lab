43bf2b8a70dcc37fda95f443cae8593829080ae6
gich2009 <waneneamos@gmail.com>
2023-03-20 01:37:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+  let found = record.find((element) => element.result === "W");
+  
+  if (found !== undefined){
+    return found.year;
+  }
+
+  return found;
+}
